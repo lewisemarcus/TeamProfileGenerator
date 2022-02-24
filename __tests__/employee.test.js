a7a151e6cbdfb5659d18d269401259912ce84116
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-02-23 21:46:01 -0800

    updated tests

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,14 +1,35 @@
 const Employee = require("../lib/employee.js")
 
-test('Test if Employee class returns correct information for each function: ', () => {
-    let empl = new Employee('Marcus Lewis', '11111', 'lewisemarcus@gmail.com')
+describe('Employee class', () => {
 
-    expect(empl.name).toEqual(expect.any(String))
-    expect(empl.id).toEqual(expect.any(Number))
-    expect(empl.email).toEqual(expect.any(String))
-    
-    expect(empl.getName()).toBe(empl.name)
-    expect(empl.getId()).toBe(empl.id)
-    expect(empl.getRole()).toBe('Employee')
-    expect(empl.getEmail()).toBe(empl.email)
+    test('Test if Employee class returns correct information for each function: ', () => {
+        const empl = new Employee('Marcus Lewis', '11111', 'lewisemarcus@gmail.com')
+        expect(empl.name).toEqual(expect.any(String))
+        expect(empl.id).toEqual(expect.any(Number))
+        expect(empl.email).toEqual(expect.any(String))
+
+        expect(empl.getName()).toBe(empl.name)
+        expect(empl.getId()).toBe(empl.id)
+        expect(empl.getRole()).toBe('Employee')
+        expect(empl.getEmail()).toBe(empl.email)
+    })
+    test('If no information is passed through, expect error', () => {
+        const cb = () => new Employee()
+        const error = new Error(
+            "Expected parameters 'name, id, email' to be a non empty strings."
+        )
+        expect(cb).toThrowError(error)
+    })
+    test('If incorrect format information is passed through, expect error', () => {
+        const cb = () => new Employee('Marcus', 'not a number', 'not an email')
+        const error = new Error("Expected correct format of parameters")
+        expect(cb).toThrowError(error)
+    })
+    test('spyon attemt', () => {
+        const empl = new Employee('Marcus Lewis', '11111', 'lewisemarcus@gmail.com')
+        const mock = jest.spyOn(empl, 'getEmail').mockImplementation(() => {})
+        empl.getEmail()
+        expect(mock).toBeCalled()
+        mock.mockRestore()
+    })
 })
